02df2b997b492bb8096241ab9bdd0909ac3ef7db
Luke Ojukwu <luke.ojukwu@gmail.com>
2018-12-20 18:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   else {
     return "I can't hear you!"
   }
-  if (string === "I love you, Grandma.!)
+  if (string === "I love you, Grandma.!")
   return 'I love you, too.'
   
   
